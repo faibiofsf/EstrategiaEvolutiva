9cbd9935139eda73735da68312dae12e451d7128
faibiofsf <ferreira06@gmail.com>
2019-05-31 08:43:07 -0300

    Implementação para diversas variáveis

--- EstrategiaEvolutiva/src/Avaliacao/AvaliacaoF6.java
+++ EstrategiaEvolutiva/src/Avaliacao/AvaliacaoF6.java
@@ -18,7 +18,7 @@ public class AvaliacaoF6 implements IAvaliacao {
 
 		double[] valoresXY = individuo.getCromossomo();
 
-		double pontuacao = ScafferF6(valoresXY[0], valoresXY[1]);
+		double pontuacao = schafferF6(valoresXY);
 
 		individuo.setFitness(pontuacao);
 
@@ -31,21 +31,25 @@ public class AvaliacaoF6 implements IAvaliacao {
 	}
 
 	// Scaffer's F6 function
-	private double ScafferF6(double x, double y) {
-		double temp1 = x * x + y * y;
+	public double schafferF6(double[] x) {
+		double temp1 = 0.0;
+		for (int i = 0; i < x.length; i++) {
+			temp1 += x[i] * x[i];
+		}
 		double temp2 = Math.sin(Math.sqrt(temp1));
 		double temp3 = 1.0 + 0.001 * temp1;
 		return (0.5 - ((temp2 * temp2 - 0.5) / (temp3 * temp3)));
+
 	}
 
 	// Expanded Scaffer's F6 function
-	private double EScafferF6(double[] x) {
-		double sum = 0.0;
-		for (int i = 1; i < x.length; i++) {
-			sum += ScafferF6(x[i - 1], x[i]);
-		}
-		sum += ScafferF6(x[x.length - 1], x[0]);
-		return (sum);
-	}
+//	private double EScafferF6(double[] x) {
+//		double sum = 0.0;
+//		for (int i = 1; i < x.length; i++) {
+//			sum += schafferF6(x[i - 1], x[i]);
+//		}
+//		sum += schafferF6(x[x.length - 1], x[0]);
+//		return (sum);
+//	}
 
 }
